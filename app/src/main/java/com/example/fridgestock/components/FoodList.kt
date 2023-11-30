b48868f66b2e4fa31b230ae8563a5cd34477c26e
rma23 <rma42685@gmail.com>
2023-11-30 18:35:22 +0900

    initial commit

--- app/src/main/java/com/example/fridgestock/components/FoodList.kt
+++ app/src/main/java/com/example/fridgestock/components/FoodList.kt
@@ -1,5 +1,7 @@
 package com.example.fridgestock.components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -7,6 +9,7 @@ import androidx.compose.runtime.Composable
 import com.example.fridgestock.MainViewModel
 import com.example.fridgestock.data.Food
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun FoodList(
     foods: List<Food>,
